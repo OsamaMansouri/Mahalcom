18a644b3332c3a491e0177ac971cb01b0a1ddb54
soumiya59 <soumiyaus@gmail.com>
2024-04-20 21:07:53 +0100

    edit model invoice

--- api/models/invoiceModel.js
+++ api/models/invoiceModel.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
-import Product from './productModel.js';
+import user from './userModel.js';
 
 
 const invoiceSchema = new mongoose.Schema({
     orderID: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
-    invoiceDate: { type: Date, required: true },
+    date: { type: Date, required: true },
     totalAmount: { type: Number, required: true },
     status: {
         type: String,
         enum: ['pending', 'paid'],
         default: 'Pending'
     },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
+    userId :{ type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 
