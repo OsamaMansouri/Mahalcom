38e7757ec33833ffc3d5f7189b2318ca2c334ed7
OsamaMansouri <mansouri.osama@gmail.com>
2024-04-20 21:19:57 +0100

    edit invoice route edit pending

--- api/models/invoiceModel.js
+++ api/models/invoiceModel.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
-import user from './userModel.js';
-
+import user from "./userModel.js";
 
 const invoiceSchema = new mongoose.Schema({
-    orderID: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
-    date: { type: Date, required: true },
-    totalAmount: { type: Number, required: true },
-    status: {
-        type: String,
-        enum: ['pending', 'paid'],
-        default: 'Pending'
-    },
-    userId :{ type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  orderID: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
+  date: { type: Date, required: true },
+  totalAmount: { type: Number, required: true },
+  status: {
+    type: String,
+    enum: ["unpaid", "pending", "paid"],
+    default: "pending",
+  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
-
-export default mongoose.model('Invoice', invoiceSchema);
+export default mongoose.model("Invoice", invoiceSchema);
