ae0001d6f36219be140dbf958ac1443cc49c2ebd
yassir003 <[moulzimelalaoui@gmail.com]>
2024-04-20 22:46:35 +0200

    update1

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ import productRoute from "./routes/productRoute.js";
 import authRoute from "./routes/authRoute.js";
 import livreurRoute from "./routes/livreurRoute.js";
 import invoiceRoute from "./routes/invoiceRoute.js";
+import clientRoute from "./routes/clientRoute.js";
+import fournisseurRoute from "./routes/fournisseurRoute.js";
 
 dotenv.config();
 
@@ -41,6 +43,8 @@ app.use("/api/user", userRoute);
 app.use("/api/stock", stockRoute);
 app.use("/api/product", productRoute);
 app.use("/api/invoice", invoiceRoute);
+app.use("/api/client",clientRoute);
+app.use("/api/fournisseur",fournisseurRoute);
 
 //routes
 app.use("/api/livreur", livreurRoute);
