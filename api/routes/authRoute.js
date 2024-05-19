3d80efbd252613caf110bdcb81cef6d4f0fa70f8
OsamaMansouri <mansouri.osama@gmail.com>
2024-05-19 17:15:38 +0100

    remobe file aand fix profile

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  logout,
+  getUserDetails,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/details", getUserDetails); // Add this line
 
 export default router;
